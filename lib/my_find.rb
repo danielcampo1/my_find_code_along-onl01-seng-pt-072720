b73ef7de9fb83e7ffa01ba060471f3289883d2a6
daniel campo <campo_daniel94@yahoo.com>
2020-08-07 13:14:37 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,13 @@
 require 'pry'
 
-def my_find(collection)
+def mdef my_find(collection)
+  i = 0
+  while i < collection.length
+  yield(collection[i]) if
+   return collection[i]
+ end
+    i = i + 1
+  end
+end
 
 end
